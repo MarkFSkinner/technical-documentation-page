786fe4d257a5479441033ca58e4dbff36512e0aa
Mark Skinner <markskinner850@gmail.com>
2019-04-25 13:04:57 -0400

    Clean up all code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,24 @@
-$(function() {
-    $(".nav-link").on('click', function() {
-        $("#navbar").removeClass("responsive");
-        $("#main-doc").removeClass("responsive");
-        //$(".icon-closed").css("display", "block");
-        //$(".icon-open").css("display", "none");
-        //$("#container").css("width", "100%");
-        //$(".line").addClass("hidden");
-        //$(".fas").removeClass("fa-times");
-        //$(".fas").addClass("fa-bars");
-    })
-})
-
-//var menuOpened = false;
-
 function toggleMenu() {
-    $("#navbar").toggleClass("responsive");
-    $("#main-doc").toggleClass("responsive");
-    /*$("#main").css("display", "inline-block");*/
-    if ($("#navbar").hasClass("responsive")) {
-        //$("#container").css("width", "50%");
-        //$(".line").removeClass("hidden");
-        //$(".fas").removeClass("fa-bars");
-        //$(".fas").addClass("fa-times");
-    } else {
-        //$("#container").css("width", "100%");
-        //$(".line").addClass("hidden");
-        //$(".fas").removeClass("fa-times");
-        //$(".fas").addClass("fa-bars");
-    }
+    $('#navbar').toggleClass('responsive');
+    $('#main-doc').toggleClass('responsive');
+}
+
+function closeMenuOnClick() {
+    $('.nav-link').on('click', function() {
+        $('#navbar').removeClass('responsive');
+        $('#main-doc').removeClass('responsive');
+    });
 }
 
 $(window).resize(function () {
-    var viewportWidth = $(window).width();
+    let viewportWidth = $(window).width();
     if (viewportWidth > 806) {
-        $("#navbar").removeClass("responsive");
-        $("#main-doc").removeClass("responsive");
-        //$(".icon-closed").css("display", "none");
-        //$(".icon-open").css("display", "none");
-        //$("#container").css("width", "100%");
-        //$(".line").addClass("hidden");
-        //$(".fas").removeClass("fa-times");
-        //$(".fas").addClass("fa-bars");
+        $('#navbar').removeClass('responsive');
+        $('#main-doc').removeClass('responsive');
     }
+});
+
+//Run on page load
+$(function() {
+    closeMenuOnClick();
 });
